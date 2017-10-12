bece224fa8ca02fedc878dd5506c34c5a04f673d
Kouson <kouson@163.com>
2017-10-12 22:27:27 +0800

    上传代码

---
+++ WebAPI.Models/Food.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAPI.Models
+{
+    public class Food
+    {
+        /// <summary>
+        /// ID
+        /// </summary>
+        public string ID { get; set; }
+
+        /// <summary>
+        /// 菜名
+        /// </summary>
+        public string Name { get; set; }
+
+
+        /// <summary>
+        /// 单位
+        /// </summary>
+        public string Unit { get; set; }
+
+        /// <summary>
+        /// 单价
+        /// </summary>
+        public decimal Price { get; set; }
+
+        /// <summary>
+        /// 描述
+        /// </summary>
+        public string Introduce   { get; set; }
+
+        /// <summary>
+        /// 评级
+        /// </summary>
+        public string Grade { get; set; }
+
+        /// <summary>
+        /// 图片路径
+        /// </summary>
+        public List<string> ImgFiles { get; set; }
+        
+        
+    }
+}
---
+++ WebAPI/MyControllers/FoodController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPI.Models;
+
+namespace WebAPI.MyControllers
+{
+    public class FoodController : ApiController
+    {
+        //
+        // GET: /Food/
+        [HttpGet]
+        public Food GetFoodByID(string ID)
+        {
+            Food food = new Food();
+            food.Name = ID + "番茄炒蛋";
+            food.Introduce = "这是最新菜肴";
+            return food;
+        
+        }
+
+    }
+}
