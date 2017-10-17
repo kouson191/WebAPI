445c122bc909b104a36a24cebab2b2512c753709
Kouson <kouson@163.com>
2017-10-17 22:44:51 +0800

    更新

--- WebAPI/MyControllers/FoodController.cs
+++ WebAPI/MyControllers/FoodController.cs
@@ -4,22 +4,47 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
-using WebAPI.Models; 
+using System.Web.Http.Filters; 
+using WebAPI.Models;
 
 namespace WebAPI.MyControllers
 {
     public class FoodController : ApiController
-    { 
+    {
         [HttpGet]
         [CrossSite]
         public Food GetFoodByID(string ID)
         {
             Food food = new Food();
+            food.ID = ID;
             food.Name = ID + "番茄炒蛋";
             food.Introduce = "这是最新菜肴";
+            food.Price = 10;
+            food.Grade = "A+";
             return food;
-        
+        }
+
+
+        [HttpGet]
+        [CrossSite]
+        public List<Food> GetAllFood()
+        {
+            List<Food> foods = new List<Food>();
+
+            for (int i = 0; i < 9999; i++)
+            {
+                Food food = new Food();
+                food.ID = i.ToString();
+                food.Name = i.ToString() + "番茄炒蛋";
+                food.Introduce = "这是最新菜肴";
+                food.Price = 10;
+                food.Grade = "A+";
+                foods.Add(food);
+            }
+            return foods;
         }
 
     }
+
+   
 }
